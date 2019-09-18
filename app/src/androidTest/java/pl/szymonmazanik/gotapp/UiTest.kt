40d4b131f313445cfa61a6f64e795103c74c97cd
szymon <smazanik106@gmail.com>
2019-09-18 19:46:06 +0100

    Instrumental test fix

--- app/src/androidTest/java/pl/szymonmazanik/gotapp/UiTest.kt
+++ app/src/androidTest/java/pl/szymonmazanik/gotapp/UiTest.kt
@@ -5,26 +5,29 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.core.IsNot.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import pl.szymonmazanik.gotapp.ui.CharacterListActivity
 import pl.szymonmazanik.gotapp.ui.CharacterListAdapter
+import timber.log.Timber
 
 class UiTest {
 
-    @Rule
-    @JvmField
-    val rule: ActivityTestRule<CharacterListActivity> =
+    @get:Rule
+    var rule: ActivityTestRule<CharacterListActivity> =
         ActivityTestRule(CharacterListActivity::class.java)
 
-    private lateinit var instrumentationContext: Context
+    lateinit var instrumentationContext: Context
 
-    private var isTablet = false
+    var isTablet = false
 
     @Before
     fun setup() {
@@ -49,17 +52,24 @@ class UiTest {
         var lastOpenedItemIndex = 0
         //Check if last opened item is the last item on list
         while (lastOpenedItemIndex < itemsCount) {
-            onView(withId(R.id.character_list)).perform(
-                RecyclerViewActions.actionOnItemAtPosition<CharacterListAdapter.ViewHolder>(
-                    lastOpenedItemIndex,
-                    click()
+                onView(withId(R.id.character_list)).perform(
+                    RecyclerViewActions.actionOnItemAtPosition<CharacterListAdapter.ViewHolder>(
+                        lastOpenedItemIndex,
+                        click()
+                    )
                 )
-            )
-            lastOpenedItemIndex++
+                lastOpenedItemIndex++
 
-            //After each click we assign new value to itemsCount in case new data came in
-            itemsCount = adapter.itemCount
-            if(!isTablet) pressBack()
+                //After each click we assign new value to itemsCount in case new data came in
+                itemsCount = adapter.itemCount
+
+                //Check if id has been set properly
+                onView(withId(R.id.character_id)).check(matches(not(withText(""))))
+
+                //Press back only on handset
+                if (!isTablet) pressBack()
+            
+                Timber.d("Items: $itemsCount Item: $lastOpenedItemIndex")
         }
     }
 }
